c044e3ae12b2b9fc2b1ecc734946c7ef4a6deab8
Pavel Pawka <pavel.chet@mail.ru>
2022-09-09 15:57:05 +0300

    kmko

--- app/src/main/java/com/example/workwithsql/MainActivity.java
+++ app/src/main/java/com/example/workwithsql/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
     public void GetTextFromSql(View v){
         TextView ID = findViewById(R.id.txtID);
         TextView Name = findViewById(R.id.txtName);
