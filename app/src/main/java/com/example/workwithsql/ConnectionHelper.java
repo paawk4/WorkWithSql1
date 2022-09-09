412eadb528e91eace57c301b88b7ca4f1b888d3a
unknown <@ngknn.local>
2022-09-09 12:33:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/workwithsql/ConnectionHelper.java
@@ -0,0 +1,35 @@
+package com.example.workwithsql;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+    String userName, userPassword, ip, port, dataBase;
+    public Connection connectionClass()
+    {
+        ip = "ngknn.ru";
+        dataBase = "43P_Chetverikov";
+        userName = "33П";
+        userPassword = "12357";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + dataBase + ";user=" + userName + ";password=" + userPassword + ";";
+            connection = DriverManager.getConnection(connectionURL);
+        }
+        catch (Exception ex){
+            Log.e("error", ex.getMessage());
+        }
+
+        return connection;
+    }
+}
