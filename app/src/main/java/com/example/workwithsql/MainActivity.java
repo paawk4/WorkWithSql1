b912c08a8c6c2ac99b935070a92c8915b901465c
ЧетвериковПВ <paawk4@gmail.com>
2022-09-17 14:52:33 +0300

    editing data processed

--- app/src/main/java/com/example/workwithsql/MainActivity.java
+++ app/src/main/java/com/example/workwithsql/MainActivity.java
@@ -3,21 +3,22 @@ package com.example.workwithsql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Message;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +28,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ListView lv = findViewById(R.id.lvDatabase);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) v).getText(),
+                        Toast.LENGTH_SHORT).show();
+                /*https://developer.alexanderklimov.ru/android/views/listview.php?ysclid=l85ua82xu5285648756#perform*/
+            }
+        });
     }
     SimpleAdapter simpleAdapter;
     public void GetList(View v)
@@ -54,19 +63,18 @@ public class MainActivity extends AppCompatActivity {
     Boolean isSuccess = false;
 
     @SuppressLint("NewApi")
-    public void ViewList(View v){
+    public void CreatePerson(View v){
         String name = "", job = "", email = "";
 
-        EditText edName = findViewById(R.id.edName);
-        EditText edJob = findViewById(R.id.edJob);
-        EditText edEmail = findViewById(R.id.edEmail);
+        EditText edName = findViewById(R.id.editName);
+        EditText edJob = findViewById(R.id.editJob);
+        EditText edEmail = findViewById(R.id.editEmail);
 
         name = edName.getText().toString();
         job = edJob.getText().toString();
         email = edEmail.getText().toString();
 
-        if(name != null && job != null && email != null){
-
+        if(name != "" || job != "" || email != ""){
             List<Map <String, String>> data = null;
             data = new ArrayList<Map <String, String>>();
             try
@@ -88,10 +96,54 @@ public class MainActivity extends AppCompatActivity {
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
-            setContentView(R.layout.activity_main);
         }
-        else{
+        setContentView(R.layout.activity_main);
+    }
 
+    public void ViewEditPerson(View v) {
+        setContentView(R.layout.edit_person);
+
+
+    }
+
+    public void EditPerson(View v){
+        String name = "", job = "", email = "";
+
+        EditText edName = findViewById(R.id.editName);
+        EditText edJob = findViewById(R.id.editJob);
+        EditText edEmail = findViewById(R.id.editEmail);
+
+        if(name != "" || job != "" || email != ""){
+            List<Map <String, String>> data = null;
+            data = new ArrayList<Map <String, String>>();
+            try
+            {
+                ConnectionHelper connectionHelper = new ConnectionHelper();
+                connection = connectionHelper.connectionClass();
+                if (connection != null){
+                    String query = " INSERT INTO Personal_Inf (name, job, email) VALUES ('" + name + "', '" + job + "', '" + email + "')";
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(query);
+
+                    ConnectionResult = "Есть контакт";
+                    isSuccess = true;
+                    connection.close();
+                }
+                else {
+                    ConnectionResult = "Не получилось";
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
+
+
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void DeletePerson(View v){
+        setContentView(R.layout.activity_main);
     }
 }
