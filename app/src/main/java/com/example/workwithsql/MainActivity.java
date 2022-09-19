03f1f9d4655c0eb1a9d101e747e1970487a6d317
Pavel Pawka <pavel.chet@mail.ru>
2022-09-19 20:49:30 +0300

    changing & delete data success

--- app/src/main/java/com/example/workwithsql/MainActivity.java
+++ app/src/main/java/com/example/workwithsql/MainActivity.java
@@ -32,8 +32,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView lv = findViewById(R.id.lvDatabase);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        View v = findViewById(com.google.android.material.R.id.ghost_view);
+        GetList(v);
+    }
+
+    SimpleAdapter simpleAdapter;
+    public void GetList(View v)
+    {
+        ListView listView = (ListView) findViewById(R.id.lvDatabase);
+
+        List<Map<String, String>> myDataList = null;
+        ListItem myData = new ListItem();
+        myDataList = myData.getList();
+
+        String[] fromView = {"Name", "Job", "Email"};
+        int[] toView = {R.id.Name, R.id.Job, R.id.Email};
+        simpleAdapter = new SimpleAdapter(MainActivity.this, myDataList,R.layout.list_template,fromView, toView);
+        listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
@@ -55,20 +72,6 @@ public class MainActivity extends AppCompatActivity {
                 findByName = nameText;
             }
         });
-    }
-    SimpleAdapter simpleAdapter;
-    public void GetList(View v)
-    {
-        ListView listView = (ListView) findViewById(R.id.lvDatabase);
-
-        List<Map<String, String>> myDataList = null;
-        ListItem myData = new ListItem();
-        myDataList = myData.getList();
-
-        String[] fromView = {"Name", "Job", "Email"};
-        int[] toView = {R.id.Name, R.id.Job, R.id.Email};
-        simpleAdapter = new SimpleAdapter(MainActivity.this, myDataList,R.layout.list_template,fromView, toView);
-        listView.setAdapter(simpleAdapter);
     }
     public void ViewCreatePerson(View v)
     {
@@ -119,31 +122,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         GetList(v);
     }
-    public void OpenEditPerson(View v){
-        ListView lv = findViewById(R.id.lvDatabase);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                String nameText, jobText, emailText;
-                TextView nameTv = (TextView)view.findViewById(R.id.Name);
-                nameText = nameTv.getText().toString();
-                TextView jobTv = (TextView)view.findViewById(R.id.Job);
-                jobText = jobTv.getText().toString();
-                TextView emailTv = (TextView)view.findViewById(R.id.Email);
-                emailText = emailTv.getText().toString();
-                setContentView(R.layout.edit_person);
-
-                TextView editName = findViewById(R.id.editName);
-                editName.setText(nameText);
-                TextView editJob = findViewById(R.id.editJob);
-                editJob.setText(jobText);
-                TextView editEmail = findViewById(R.id.editEmail);
-                editEmail.setText(emailText);
-                findByName = nameText;
-            }
-        });
-    }
     public void EditPerson(View v){
         String name = "", job = "", email = "";
 
@@ -156,8 +134,6 @@ public class MainActivity extends AppCompatActivity {
         email = edEmail.getText().toString();
 
         if(name != "" || job != "" || email != ""){
-            List<Map <String, String>> data = null;
-            data = new ArrayList<Map <String, String>>();
             try
             {
                 ConnectionHelper connectionHelper = new ConnectionHelper();
@@ -179,14 +155,28 @@ public class MainActivity extends AppCompatActivity {
                 throwables.printStackTrace();
             }
         }
-
-
-
         setContentView(R.layout.activity_main);
-
-
+        GetList(v);
     }
     public void DeletePerson(View v){
+            try
+            {
+                ConnectionHelper connectionHelper = new ConnectionHelper();
+                connection = connectionHelper.connectionClass();
+                if (connection != null){
+                    String query = "DELETE Personal_Inf WHERE name = '" + findByName + "'";
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(query);
+
+                    ConnectionResult = "Есть контакт";
+                    connection.close();
+                }
+                else {
+                    ConnectionResult = "Не получилось";
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         setContentView(R.layout.activity_main);
         GetList(v);
     }
