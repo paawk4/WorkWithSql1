322f8c48b24a5481c1bdd18c97385ed510a3e02e
Pavel Pawka <pavel.chet@mail.ru>
2022-10-08 20:30:55 +0300

    1.0

--- app/src/main/java/com/example/workwithsql/MainActivity.java
+++ app/src/main/java/com/example/workwithsql/MainActivity.java
@@ -8,8 +8,8 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -28,7 +28,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
@@ -66,123 +65,161 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         SpinnerInit();
     }
 
+    public void SpinnerInit(){
+        try {
+            Spinner spinner = findViewById(R.id.spinnerSort);
+            spinner.setOnItemSelectedListener(this);
+
+            List<String> categories = new ArrayList<>();
+            categories.add("By name a-z");
+            categories.add("By position a-z");
+
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinner.setAdapter(dataAdapter);
+        }
+        catch (Exception ignored){
+        }
+    }
+
     public void DbOperations(View v) {
-        Button btnCreate = findViewById(R.id.btnCreate);
-        Button btnEdit = findViewById(R.id.btnEdit);
-        Button btnDelete = findViewById(R.id.btnDelete);
+        try {
+            Button btnCreate = findViewById(R.id.btnCreate);
+            Button btnEdit = findViewById(R.id.btnEdit);
+            Button btnDelete = findViewById(R.id.btnDelete);
 
-        EditText edName = findViewById(R.id.editName);
-        EditText edJob = findViewById(R.id.editJob);
-        EditText edEmail = findViewById(R.id.editEmail);
-        ImageView ivAvatar = findViewById(R.id.ivAvatar);
+            EditText edName = findViewById(R.id.editName);
+            EditText edJob = findViewById(R.id.editJob);
+            EditText edEmail = findViewById(R.id.editEmail);
+            ImageView ivAvatar = findViewById(R.id.ivAvatar);
 
-        String name = edName.getText().toString();
-        String job = edJob.getText().toString();
-        String email = edEmail.getText().toString();
+            String name = edName.getText().toString();
+            String job = edJob.getText().toString();
+            String email = edEmail.getText().toString();
 
-        if (!name.equals("") & !job.equals("") & !email.equals("") & !((BitmapDrawable) ivAvatar.getDrawable()).getBitmap().toString().equals("")) {
-            Bitmap avatarBm = ((BitmapDrawable) ivAvatar.getDrawable()).getBitmap();
+            if (!name.equals("") & !job.equals("") & !email.equals("") & !((BitmapDrawable) ivAvatar.getDrawable()).getBitmap().toString().equals("")) {
+                Bitmap avatarBm = ((BitmapDrawable) ivAvatar.getDrawable()).getBitmap();
 
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            avatarBm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                avatarBm.compress(Bitmap.CompressFormat.PNG, 100, stream);
 
-            byte[] byteArray = stream.toByteArray();
+                byte[] byteArray = stream.toByteArray();
 
-            String encodedImageUpdate = Base64.encodeToString(byteArray, Base64.DEFAULT);
-            try {
-                connection = connectionHelper.connectionClass();
-                String query = "";
-                if (v.equals(btnCreate)) {
-                    query = "INSERT INTO Personal_Inf (name, job, email, image) VALUES ('" + name
-                            + "', '" + job + "', '" + email + "','" + encodedImage + "')";
-                } else if (v.equals(btnEdit)) {
-                    query = "UPDATE Personal_Inf\n" +
-                            "SET name = '" + name + "', job = '" + job + "', email = '" + email + "', image = '" + encodedImageUpdate + "'\n" +
-                            "WHERE name = '" + findByName + "'";
-                } else if (v.equals(btnDelete)) {
-                    query = "DELETE Personal_Inf WHERE name = '" + findByName + "'";
+                String encodedImageUpdate = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                try {
+                    connection = connectionHelper.connectionClass();
+                    String query = "";
+                    if (v.equals(btnCreate)) {
+                        query = "INSERT INTO Personal_Inf (name, job, email, image) VALUES ('" + name
+                                + "', '" + job + "', '" + email + "','" + encodedImage + "')";
+                    } else if (v.equals(btnEdit)) {
+                        query = "UPDATE Personal_Inf\n" +
+                                "SET name = '" + name + "', job = '" + job + "', email = '" + email + "', image = '" + encodedImageUpdate + "'\n" +
+                                "WHERE name = '" + findByName + "'";
+                    } else if (v.equals(btnDelete)) {
+                        query = "DELETE Personal_Inf WHERE name = '" + findByName + "'";
+                    }
+                    Statement statement = connection.createStatement();
+                    statement.executeQuery(query);
+                    connection.close();
+                } catch (SQLException throwable) {
+                    throwable.printStackTrace();
                 }
-                Statement statement = connection.createStatement();
-                statement.executeQuery(query);
-                connection.close();
-            } catch (SQLException throwable) {
-                throwable.printStackTrace();
+                setContentView(R.layout.activity_main);
+                SpinnerInit();
+                ListOperations(v);
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Введите данные", Toast.LENGTH_SHORT);
+                toast.show();
             }
-            setContentView(R.layout.activity_main);
-            SpinnerInit();
-            ListOperations(v);
-        } else {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "Введите данные", Toast.LENGTH_SHORT);
-            toast.show();
+        }
+        catch (Exception ex){
+            Toast.makeText(v.getContext(), ex.toString(), Toast.LENGTH_LONG).show();
         }
     }
 
     @SuppressWarnings("deprecation")
     public void ImageView(View v) {
-        Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        chooseFileIntent.setType("*/*");
-        chooseFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            chooseFileIntent.setType("*/*");
+            chooseFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
 
-        chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose a file");
-        startActivityForResult(chooseFileIntent, MY_RESULT_CODE_FILECHOOSER);
+            chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose a file");
+            startActivityForResult(chooseFileIntent, MY_RESULT_CODE_FILECHOOSER);
+        }
+        catch (Exception ex){
+            Toast.makeText(v.getContext(), ex.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MY_RESULT_CODE_FILECHOOSER) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (data != null) {
-                    Uri fileUri = data.getData();
-                    Log.i(LOG_TAG, "Uri: " + fileUri);
+        try {
+            if (requestCode == MY_RESULT_CODE_FILECHOOSER) {
+                if (resultCode == Activity.RESULT_OK) {
+                    if (data != null) {
+                        Uri fileUri = data.getData();
+                        Log.i(LOG_TAG, "Uri: " + fileUri);
 
-                    String filePath;
-                    try {
-                        filePath = FileUtils.getPath(this.getBaseContext(), fileUri);
-                        ImageView image = findViewById(R.id.ivAvatar);
-                        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                        String filePath;
+                        try {
+                            filePath = FileUtils.getPath(this.getBaseContext(), fileUri);
+                            ImageView image = findViewById(R.id.ivAvatar);
+                            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
 
-                        ByteArrayOutputStream outStream = BitmapCompress.BitmapCompressor(bitmap);
+                            ByteArrayOutputStream outStream = BitmapCompress.BitmapCompressor(bitmap);
 
-                        image.setImageBitmap(bitmap);
+                            image.setImageBitmap(bitmap);
 
-                        byte[] byteArray = outStream.toByteArray();
+                            byte[] byteArray = outStream.toByteArray();
 
-                        encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
-                    } catch (Exception e) {
-                        Log.e(LOG_TAG, "Error: " + e);
-                        Toast.makeText(this.getBaseContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                            encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                        } catch (Exception e) {
+                            Log.e(LOG_TAG, "Error: " + e);
+                            Toast.makeText(this.getBaseContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
             }
+            super.onActivityResult(requestCode, resultCode, data);
         }
-        super.onActivityResult(requestCode, resultCode, data);
+        catch (Exception ignored){
+        }
     }
 
     public void Search(View v) {
-        EditText searchBar = findViewById(R.id.searchBar);
-        TextView tw = findViewById(R.id.textView2);
-        if(searchBar.isFocused()){
-            searchBar.setVisibility(View.GONE);
-            tw.setVisibility(View.VISIBLE);
-        }else{
-            searchBar.setVisibility(View.VISIBLE);
-            tw.setVisibility(View.GONE);
+        try {
+            EditText searchBar = findViewById(R.id.searchBar);
+            TextView tw = findViewById(R.id.textView2);
+            if(searchBar.isFocused()){
+                searchBar.setVisibility(View.GONE);
+                tw.setVisibility(View.VISIBLE);
+            }else{
+                searchBar.setVisibility(View.VISIBLE);
+                tw.setVisibility(View.GONE);
 
-            searchBar.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                }
+                searchBar.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    }
 
-                @Override
-                public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-                    profileAdapter.getFilter().filter(s.toString());
-                }
+                    @Override
+                    public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                        profileAdapter.getFilter().filter(s.toString());
+                    }
 
-                @Override
-                public void afterTextChanged(Editable editable) {
-                }
-            });
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                    }
+                });
+            }
+        }
+        catch (Exception ex){
+            Toast.makeText(v.getContext(), ex.toString(), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -238,67 +275,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     public void Navigation(View v) {
-        Button viewCreate = findViewById(R.id.btnViewCreate);
-        Button goBack = findViewById(R.id.btnGoBack);
-        if (v.equals(viewCreate)) {
-            setContentView(R.layout.create_person);
-        } else if (v.equals(goBack)) {
-            setContentView(R.layout.activity_main);
-            ListOperations(v);
-            SpinnerInit();
+        try {
+            Button viewCreate = findViewById(R.id.btnViewCreate);
+            Button goBack = findViewById(R.id.btnGoBack);
+            if (v.equals(viewCreate)) {
+                setContentView(R.layout.create_person);
+            } else if (v.equals(goBack)) {
+                setContentView(R.layout.activity_main);
+                ListOperations(v);
+                SpinnerInit();
+            }
         }
-    }
-    public void SpinnerInit(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerSort);
-        spinner.setOnItemSelectedListener(this);
-
-        List<String> categories = new ArrayList<String>();
-        categories.add("By name a-z");
-        categories.add("By position a-z");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(dataAdapter);
-        spinner.setBackgroundResource(R.drawable.spinner_bg);
-    }
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        ListView listViewDB = findViewById(R.id.lvDatabase);
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-        if(item == "By name a-z")
-        {
-            Collections.sort(profileList, new Comparator<Profile>() {
-                @Override
-                public int compare(Profile o1, Profile o2) {
-                    return o1.name.toLowerCase(Locale.ROOT).compareTo(o2.name.toLowerCase(Locale.ROOT));
-                }
-            });
-            profileAdapter = new ProfileAdapter(MainActivity.this, profileList);
-            listViewDB.setAdapter(profileAdapter);
+        catch (Exception ex){
+            Toast.makeText(v.getContext(), ex.toString(), Toast.LENGTH_LONG).show();
         }
-        else if(item == "By position a-z")
-        {
-            Collections.sort(profileList, new Comparator<Profile>() {
-                @Override
-                public int compare(Profile o1, Profile o2) {
-                    return o1.job.toLowerCase(Locale.ROOT).compareTo(o2.job.toLowerCase(Locale.ROOT));
-                }
-            });
-            profileAdapter = new ProfileAdapter(MainActivity.this, profileList);
-            listViewDB.setAdapter(profileAdapter);
-        }
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerSort);
-
-        spinner.setBackgroundResource(R.drawable.spinner_bg);
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        try {
+            ListView listViewDB = findViewById(R.id.lvDatabase);
+            String item = parent.getItemAtPosition(position).toString();
+            if(item.equals("By name a-z")){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    Collections.sort(profileList, Comparator.comparing(o -> o.name.toLowerCase(Locale.ROOT)));
+                }
+                profileAdapter = new ProfileAdapter(MainActivity.this, profileList);
+                listViewDB.setAdapter(profileAdapter);
+            }
+            else if(item.equals("By position a-z")){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    Collections.sort(profileList, Comparator.comparing(o -> o.job.toLowerCase(Locale.ROOT)));
+                }
+                profileAdapter = new ProfileAdapter(MainActivity.this, profileList);
+                listViewDB.setAdapter(profileAdapter);
+            }
+        }
+        catch (Exception ex){
+            Toast.makeText(parent.getContext(), ex.toString(), Toast.LENGTH_LONG).show();
+        }
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 }
